64650915d04e465a74c449b69e3899f36bada9a7
Juan <juan.technova@gmail.com>
2024-05-01 17:41:00 -0500

    viva vo

--- src/main/java/ece448/iot_hub/GroupsModel.java
+++ src/main/java/ece448/iot_hub/GroupsModel.java
@@ -1,5 +1,6 @@
 package ece448.iot_hub;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -9,22 +10,48 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class GroupsModel {
-	private HashMap<String, HashSet<String>> groups = new HashMap<>();
+    private HashMap<String, HashSet<String>> groups;
 
-	synchronized public List<String> getGroups() {
-		return new ArrayList<>(groups.keySet());
-	}
+    public GroupsModel() {
+        this.groups = loadGroupsFromFile();
+    }
 
-	synchronized public List<String> getGroupMembers(String group) {
-		HashSet<String> members = groups.get(group);
-		return (members == null)? new ArrayList<>(): new ArrayList<>(members);
-	}
+    synchronized public List<String> getGroups() {
+        return new ArrayList<>(groups.keySet());
+    }
 
-	synchronized public void setGroupMembers(String group, List<String> members) {
-		groups.put(group, new HashSet<>(members));
-	}
+    synchronized public List<String> getGroupMembers(String group) {
+        HashSet<String> members = groups.get(group);
+        return (members == null) ? new ArrayList<>() : new ArrayList<>(members);
+    }
 
-	synchronized public void removeGroup(String group) {
-		groups.remove(group);
-	}
+    synchronized public void setGroupMembers(String group, List<String> members) {
+        groups.put(group, new HashSet<>(members));
+        saveGroupsToFile(groups);
+    }
+
+    synchronized public void removeGroup(String group) {
+        groups.remove(group);
+        saveGroupsToFile(groups);
+    }
+
+    private HashMap<String, HashSet<String>> loadGroupsFromFile() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("groups.ser"))) {
+            return (HashMap<String, HashSet<String>>) ois.readObject();
+        } catch (FileNotFoundException e) {
+            // If file not found, return an empty HashMap
+            return new HashMap<>();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return new HashMap<>();
+        }
+    }
+
+    private void saveGroupsToFile(HashMap<String, HashSet<String>> groups) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("groups.ser"))) {
+            oos.writeObject(groups);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
